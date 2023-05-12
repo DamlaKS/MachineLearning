6be07f482bca3129e73ebc8552f3723e0cbbfcd9
Damla KS <damlaksrbst@gmail.com>
2023-05-12 15:58:03 +0300

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Damla KS on 12.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
